db1cd039f53ce47066bd9e9c595ef475b9a5c49d
Lily <lily.yezeng@gmail.com>
2026-05-20 11:44:01 -0700

    Implement constructor for HTMLManager
    Added constructor to initialize HTMLManager with a queue of HTMLTags.

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,13 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
+  public HTMLManager(Queue<HTMLTag> html) { 
+    if(html == null) {
+      throw new IllegalArgumentException();
+    }
+    tags = new LinkedList<HTMLTags>();
+    for(HTMLTags: html) {
+      tags.add(tag);  
+  }
+  
 }
